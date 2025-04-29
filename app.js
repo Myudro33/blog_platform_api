d32abc9e601a0ca197df2dde10737f262bd1444b
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-29 18:09:38 +0400

    chore: setup app

---
+++ app.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to postgress" });
+});
+export default app;
