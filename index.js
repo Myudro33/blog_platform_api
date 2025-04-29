d935efde6838065a7d621d9c8fba2b9941b86169
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-29 19:38:04 +0400

    fix: make uploads folder static

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
+app.use("/uploads", express.static("./uploads"));
 app.listen(3000, () => {
   console.log("server is running on", 3000);
 });
