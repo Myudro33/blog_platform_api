840cc51c5b648dc5764ba008985aa169253492f6
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-30 19:45:02 +0400

    feat: create swagger specs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import specs from "./models/swagger.js";
 dotenv.config();
 app.use("/uploads", express.static("./uploads"));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.listen(3000, () => {
   console.log("server is running on", 3000);
 });
