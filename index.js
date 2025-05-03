5237196b01a123fb38a5b16d0cc1b8d601059c9f
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-03 14:15:23 +0400

    fix: update environment variable for server port in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import specs from "./models/swagger.js";
 dotenv.config();
 app.use("/uploads", express.static("./uploads"));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-const port = process.env.PROJECT_PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("server is running on", port);
 });
