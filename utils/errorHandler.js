7fed6c45c73885dbb731c488581bfa5fb5cc5548
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-04 14:27:01 +0400

    fix: enhance error handling in production and development environments

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -10,10 +10,30 @@ export class AppError extends Error {
 export const handleError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
+  if (!err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: "Error",
+      message: "Internal Server Error!",
+    });
+  }
+  if (process.env.NODE_ENV === "development") {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // Programming or other unknown error: don't leak error details
+    console.error("ERROR 💥", err);
+    // Send generic error response
+    err.message = "Something went very wrong!";
+  }
 };
