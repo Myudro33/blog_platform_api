cbe2bbcabc12e6ed02e35e490b860cb2978aae59
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-05 16:56:06 +0400

    fix: move swagger route in app js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 import { handleError } from "./utils/errorHandler.js";
+import swaggerUI from "swagger-ui-express";
+import specs from "./models/swagger.js";
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -13,6 +15,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 app.all("*", (req, res) => {
   res.status(404).json({
     status: "fail",
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import express from "express";
-import swaggerUI from "swagger-ui-express";
-import specs from "./models/swagger.js";
+
 import cors from "cors";
 dotenv.config();
 app.use("/uploads", express.static("./uploads"));
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use(cors());
 
 const port = process.env.PORT || 3000;
